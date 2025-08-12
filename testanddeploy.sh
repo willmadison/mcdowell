26ed1406e3b42b7bccae7f48cc22a286e5a1b5ce
willmadison <wmadisonDev@gmail.com>
2025-08-12 09:34:04 -0400

    removing TravisCI references from the testanddeploy script

--- testanddeploy.sh
+++ testanddeploy.sh
@@ -10,6 +10,7 @@ PROJECT_DIR=${PWD}
 CONTAINER_GOPATH='/go'
 CONTAINER_PROJECT_DIR="${CONTAINER_GOPATH}/src/${PROJECT_NAME}"
 CONTAINER_PROJECT_GOPATH="${CONTAINER_GOPATH}"
+BUILD_VERSION=${CIRCLE_BUILD_NUM}.$((CIRCLE_NODE_INDEX + 1))
 
 docker run --rm \
     --net="host" \
@@ -27,15 +28,15 @@ docker run --rm \
         -e CGO_ENABLED=0 \
         -w "${CONTAINER_PROJECT_DIR}" \
         golang:1.10.0-alpine \
-        go build -v -ldflags "-X main.version=${TRAVIS_JOB_NUMBER}" ${PROJECT_NAME}/cmd/mcdowell
+        go build -v -ldflags "-X main.version=${BUILD_VERSION}" ${PROJECT_NAME}/cmd/mcdowell
 
 docker build -f ${PROJECT_DIR}/Dockerfile \
-    -t ${CONTAINER_NAME}:${TRAVIS_JOB_NUMBER} \
+    -t ${CONTAINER_NAME}:${BUILD_VERSION} \
     "${PROJECT_DIR}"
 
 rm -f "${PROJECT_DIR}/mcdowell"
 
-docker tag ${CONTAINER_NAME}:${TRAVIS_JOB_NUMBER} ${CONTAINER_NAME}:latest
+docker tag ${CONTAINER_NAME}:${BUILD_VERSION} ${CONTAINER_NAME}:latest
 
 sudo gcloud docker -- push gcr.io/atlblacktech-slack-bot/mcdowell
-sudo kubectl set image deployment/atlblacktech-slack-bot atlblacktech-slack-bot=gcr.io/atlblacktech-slack-bot/mcdowell:${TRAVIS_JOB_NUMBER}
+sudo kubectl set image deployment/atlblacktech-slack-bot atlblacktech-slack-bot=gcr.io/atlblacktech-slack-bot/mcdowell:${BUILD_VERSION}
